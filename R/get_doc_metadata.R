77306a0f48d558e44052e8c2a29b05c3830e902c
Tony Fujs <tony.fujs@gmail.com>
2017-05-17 16:26:24 -0400

    Add get_doc_metadata()

---
+++ R/get_doc_metadata.R
@@ -0,0 +1,16 @@
+#' get_doc_metadata
+#'
+#' @param id numeric: Survey Unique ID
+#' @param token character
+#'
+#' @return list
+#' @export
+#'
+
+get_doc_metadata <- function(id, token){
+  path <- paste0('index.php/api/v2/metadata/doc_dscr/', id)
+  out <- connect_mdlib(path = path, token = token)
+  out <- out$content
+
+  return(out)
+}
