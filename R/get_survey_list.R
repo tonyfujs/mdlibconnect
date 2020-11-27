4993ce656251df23ed967268b942002eaf8adef6
Tony Fujs <tfujs@worldbank.org>
2020-11-27 16:25:06 +0100

    fix get_survey_list()
    max number of records was hard coded

--- R/get_survey_list.R
+++ R/get_survey_list.R
@@ -7,8 +7,33 @@
 #'
 
 get_survey_list <- function(token){
-  out <- connect_mdlib(path = 'index.php/api/catalog/search?ps=10000', token = token)
+
+  n_records <- get_number_of_surveys(token)
+  path <- paste0('index.php/api/catalog/search?ps=', n_records)
+  out <- connect_mdlib(path = path, token = token)
   out <- out$content$result$rows
 
   return(out)
+}
+
+get_number_of_surveys <- function(token) {
+  # Should be better integrated with the other functions (URL should not be hard coded)
+  url <- "http://microdatalib.worldbank.org/index.php/api/catalog/search?ps=1"
+
+  resp <- httr::GET(url,
+                    httr::add_headers(.headers = c('X-API-KEY' = token,
+                                                   'charset' = "utf-8")),
+                    httr::accept_json())
+
+  # Return useful message on error
+  httr::stop_for_status(resp, task = 'complete request to Microdata library API\n')
+  # CHECK: datatype is .JSON
+  if (httr::http_type(resp) != "application/json") {
+    stop("API did not return json", call. = FALSE)
+  }
+  # Parse response
+  parsed <- jsonlite::fromJSON(httr::content(resp, "text"), simplifyVector = FALSE)
+  n_records <- parsed$result$total
+
+  return(n_records)
 }
